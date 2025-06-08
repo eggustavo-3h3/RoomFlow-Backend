658b08f04459bf07e3624ac680e55af06d6c4bbb
Eduardo Professor <eggustavo+3h3@gmail.com>
2025-06-08 01:41:00 -0300

    Remove StatusSala property from Sala entity
    This commit introduces a migration class `RemovidoStatusSalaDaEntidadeSala` that removes the `StatusSala` column from the `TAB_Sala` table. The `Up` method drops the column, while the `Down` method allows for its restoration if needed. The `RoomFlowContextModelSnapshot.cs` file has also been updated to reflect this change, ensuring the database schema is in sync with the updated entity model.

---
+++ RoomFlowApi/Migrations/20250608044024_RemovidoStatusSalaDaEntidadeSala.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoomFlowApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovidoStatusSalaDaEntidadeSala : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusSala",
+                table: "TAB_Sala");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StatusSala",
+                table: "TAB_Sala",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
