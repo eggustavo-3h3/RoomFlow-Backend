757b490fc9a50af6b19e08778a9cd3a04922723a
Eduardo Professor <eggustavo+3h3@gmail.com>
2025-06-08 15:12:10 -0300

    Add new room types to EnumTipoSala
    Updated the `EnumTipoSala` enum to include additional room types: `LabEnfermagem`, `LabEletrica`, `LabMecatronica`, `Oficina`, `Quadra`, and `SalaMaker`, with values ranging from 4 to 10. The existing `SalaComum` entry remains unchanged.

--- RoomFlowApi/Domain/Enumerators/EnumTipoSala.cs
+++ RoomFlowApi/Domain/Enumerators/EnumTipoSala.cs
@@ -4,6 +4,13 @@
     {
         LabInformatica = 1,
         LabQuimica = 2,
-        SalaComum = 3
+        SalaComum = 3,
+        LabEnfermagem = 4,
+        LabEletrica = 5,
+        LabMecatronica = 6,
+        Oficina = 7,
+        Quadra = 8,
+        SalaMaker = 9,
+        Biblioteca = 10,
     }
 }
